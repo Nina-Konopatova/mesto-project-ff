41fbe07a8e286c7e34ef2bc8385ea6a53572d550
Nina Konopatova <konopatova.nk@gmail.com>
2024-02-10 21:04:15 +0300

    Место

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,24 @@
 // @todo: Темплейт карточки
 
+const cardList = document.querySelector('.places__list');
+
+function addCard(nameValue, linkValue) {
+    const cardTemplate = document.querySelector('#card-template'). content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = linkValue;
+    cardElement.querySelector('.card__title').textContent  = nameValue;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function () {
+        cardElement.remove();
+    });
+
+    cardList.append(cardElement);
+}
+
+initialCards.forEach(element => {
+    addCard(element.name, element.link);
+});
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
