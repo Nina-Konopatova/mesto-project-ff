1d1360e0530437b698c6275998f21e63157a81a8
Nina Konopatova <konopatova.nk@gmail.com>
2024-02-13 19:57:34 +0300

    Конопатова_Место3

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,20 @@
 const cardList = document.querySelector('.places__list');
 
 function addCard(data, onDelete) {
-    
+
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
     cardElement.querySelector('.card__image').src = data.link;
     cardElement.querySelector('.card__title').textContent  = data.name;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
-        onDelete(cardElement);
-    });
-
+    cardElement.querySelector('.card__delete-button').addEventListener('click', onDelete);
+    
     return cardElement;    
 }
 
-const cardDelete = item => {
-    item.remove();
-} 
+function cardDelete(evt) {
+    evt.target.closest(".card").remove();
+  } 
 
 initialCards.forEach(element => {
     const card = addCard(element, cardDelete);
